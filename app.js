bb4cff8481793452f46a07e699597f9fae0bf168
rodrigo-vilas <rodrigovilas84@gmail.com>
2026-09-20 19:21:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = 'inutry2024';
+const MAKE_WEBHOOK_URL = 'https://hook.us2.make.com/zgqo6kphj7u4iksqdt4c3n16nvvbk1sy';
 
 app.get('/', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
-  
+
   if (mode === 'subscribe' && token === VERIFY_TOKEN) {
     res.status(200).send(challenge);
   } else {
@@ -16,8 +17,18 @@ app.get('/', (req, res) => {
   }
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   res.sendStatus(200);
+  
+  try {
+    await fetch(MAKE_WEBHOOK_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body)
+    });
+  } catch (err) {
+    console.error('Erro ao encaminhar para Make:', err);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
